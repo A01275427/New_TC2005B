ffe10fdf8db614d2f8d0e319ea059cc01082bba7
FaKeArtur27 <arsanmx@gmail.com>
2023-09-15 11:10:03 -0600

    Archivos de paigna

---
+++ motocicletas/primeros labs/intro.js
@@ -0,0 +1,19 @@
+
+console.log("holaaa")
+
+const precio = 50;
+console.log(precio)
+
+
+function vamonos() {
+    console.log("¡Vámonos, ya se acabó la clase!");
+}
+
+vamonos();
+
+
+const despliega_rating = () => {
+    const rating = "10/10 ¡Excelente!";
+    oppenheimer.innerHTML = rating;
+    oppenheimer.onclick = despliega_nombre;
+}
