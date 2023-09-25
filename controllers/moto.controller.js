b758f252c464b8181aa122fe4e0cbe1bc74b97ef
FaKeArtur27 <arsanmx@gmail.com>
2023-09-25 10:26:23 -0600

    errores en lab13

--- controllers/moto.controller.js
+++ controllers/moto.controller.js
@@ -3,21 +3,29 @@ const modelo = require('../models/moto.model');
 
 exports.get_add = (request, response, next) => {
     response.render('motocicletas/add.ejs')
+    
+    const allMotos = motocicletas.fetchAll();
+    response.render('motocicletas/list.ejs', { motocicletas: allMotos });
 };
+
+
 
 exports.post_add = (request, response, next) => {
     console.log(request.body);
 
-    const motocicletas = new motocicletas({
+    const newMoto = new motocicletas({
         nombre: request.body.nombre,
         imagen: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/ed67ae20658687.562eefc31f60a.jpg",
         descripcion: request.body.descripcion,
     });
+
+    newMoto.save();
+
     response.redirect('/motocicletas')
 }
 
 exports.get_list = (request, response, next) => {
     response.render('motocicletas/list.ejs', { 
-        motocicletas: motocicletas
+        motocicletas: moto.fetchAll()
     });
 };
--- routes/moto.routes.js
+++ routes/moto.routes.js
@@ -6,7 +6,6 @@ const motocicletasController = require('../controllers/moto.controller');
 
 
 router.get('/add', motocicletasController.get_add);
-
 router.post('/add', motocicletasController.post_add);
 router.get('/list', motocicletasController.get_list);
 router.get('/', motocicletasController.get_list);
