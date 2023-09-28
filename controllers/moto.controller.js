af3eab5964bfb1bd0bc97892e590d1eb9cf2f3df
ArturoSR <a01275427@tec.mx>
2023-09-28 15:45:02 -0600

    algo

--- controllers/moto.controller.js
+++ controllers/moto.controller.js
@@ -41,3 +41,28 @@ exports.get_list = (request, response, next) => {
     response.render('motocicletas/list.ejs', { marcas: allMotos });
 };
 */
+
+exports.get_list = (request, response, next) => {
+    const cookie = request.get('Cookie');
+    let ultimo_acceso;
+    let tiempo_transcurrido;
+
+    if (cookie && cookie.includes('ultimo_acceso')) {
+        const fechaString = cookie.split('ultimo_acceso=')[1].split(';')[0];
+        ultimo_acceso = new Date(fechaString);
+
+        if (!isNaN(ultimo_acceso.getTime())) {
+            tiempo_transcurrido = (new Date().getTime() - ultimo_acceso.getTime()) / 1000;
+            console.log(tiempo_transcurrido);
+        } else {
+            console.log('Fecha de último acceso no válida');
+        }
+    } else {
+        console.log('No hay cookie de último acceso');
+    }
+
+    response.render('motocicletas/list.ejs', { 
+        marcas: motocicletas.fetchAll(),
+        tiempo_transcurrido: tiempo_transcurrido || 'Desconocido'
+    });
+};
