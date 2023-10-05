85434b694a0cab21720fd41e203112f98aeaafa8
ArturoSR <a01275427@tec.mx>
2023-10-05 03:29:04 -0600

    sql del rbac

--- sql/usuarios.sql
+++ sql/usuarios.sql
@@ -4,6 +4,30 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+--Estructura de tabla para privilegios
+
+CREATE TABLE `privilegios` (
+    `id` int (11) NOT NULL,
+    `nombre` varchar(100) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--Estructura de tabla para roles
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--Estructura para tabla rol_privilegios
+
+CREATE TABLE `rol_privilegio` (
+  `id_rol` int(11) NOT NULL,
+  `id_privilegio` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--Estructura para tabla usuarios
+
 CREATE TABLE `usuarios` (
     `id` int (11) NOT NULL,
     `username` varchar(40) NOT NULL,
@@ -12,10 +36,59 @@ CREATE TABLE `usuarios` (
     `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--Indices para tabla privilegios
+
+ALTER TABLE `privilegios`
+  ADD PRIMARY KEY(`id`);
+
+--Indices para tabla roles
+
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--Indices para tabla rol_privilegio
+
+ALTER TABLE `rol_privilegio`
+  ADD PRIMARY KEY (`id_rol`,`id_privilegio`),
+  ADD KEY `id_privilegio` (`id_privilegio`);
+
+--Indices para tabla usuarios
+
 ALTER TABLE `usuarios`
     ADD PRIMARY KEY (`id`),
     ADD UNIQUE KEY `username_unique` (`username`);
 
+--Indices para tabla usuario_rol
+
+ALTER TABLE `usuario_rol`
+  ADD PRIMARY KEY (`id_usuario`,`id_rol`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--AUTO_INCREMENT de la tabla privilegios
+
+ALTER TABLE `privilegios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- AUTO_INCREMENT de la tabla `roles`
+
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `usuarios`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Restricciones para tablas volcadas
+
+-- Filtros para la tabla `rol_privilegio`
+
+ALTER TABLE `rol_privilegio`
+  ADD CONSTRAINT `rol_privilegio_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`),
+  ADD CONSTRAINT `rol_privilegio_ibfk_2` FOREIGN KEY (`id_privilegio`) REFERENCES `privilegios` (`id`);
+
+-- Filtros para la tabla `usuario_rol`
+
+ALTER TABLE `usuario_rol`
+  ADD CONSTRAINT `usuario_rol_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuario_rol_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
+
 COMMIT;
