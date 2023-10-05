297743fd41f5abf2f2af703c8f6a4bf464d9eec7
ArturoSR <a01275427@tec.mx>
2023-10-05 03:11:59 -0600

    Proteccion csrf

--- routes/moto.routes.js
+++ routes/moto.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 const motocicletasController = require('../controllers/moto.controller');
 
-router.get('/add', motocicletasController.get_add);
-router.post('/add', motocicletasController.post_add);
-router.get('/list', motocicletasController.get_list);
-router.get('/list/:id', motocicletasController.get_list);
-router.get('/:id', motocicletasController.get_list);
-router.get('/', motocicletasController.get_list);
+router.get('/add', isAuth, motocicletasController.get_add);
+router.post('/add', isAuth, motocicletasController.post_add);
+router.get('/list', isAuth, motocicletasController.get_list);
+router.get('/list/:id', isAuth, motocicletasController.get_list);
+router.get('/:id', isAuth, motocicletasController.get_list);
+router.get('/', isAuth, motocicletasController.get_list);
 
 module.exports = router;
